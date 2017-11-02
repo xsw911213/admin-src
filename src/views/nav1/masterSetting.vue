2472b07b22443790cc06ae26fb9ef795bfe66d63
xushaowei <xushaowei10077@autohome.com.cn>
2017-11-02 17:49:37 +0800

    first commit

---
+++ src/views/nav1/masterSetting.vue
@@ -0,0 +1,28 @@
+<template>
+	<section>
+		<!--工具条-->
+		这是用户管理页面
+
+	</section>
+</template>
+<script>
+	//import NProgress from 'nprogress'
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+
+		},
+		mounted() {
+
+		}
+	};
+
+</script>
+
+<style scoped>
+
+</style>
